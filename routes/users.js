51e85071763b8e5a657945902739244d4748d831
Wassim Slim <53937598+WassimSlim21@users.noreply.github.com>
2024-06-04 11:15:41 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  console.log("Hello Test Commit", req);
+  console.log("Hello Test Commit");
+  console.log("modified by wassim SLIM21");
+  
   res.send('respond with a resource');
 });
 
