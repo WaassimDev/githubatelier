096e3efafce335da052c2a11c192ac4526ea4fb1
WaassimDev <wassim.slim@aziza.tn>
2024-06-04 11:05:11 +0100

    add log

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  console.log("Hello Test Commit");
   res.send('respond with a resource');
 });
 
