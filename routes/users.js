cb5b128da8e401e92f9bd3f7142c3be55c68a4cb
WaassimDev <wassim.slim@aziza.tn>
2024-06-04 11:16:20 +0100

    add some modifivation

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,8 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   console.log("Hello Test Commit", req);
+  var x = 5+3;
+  
   res.send('respond with a resource');
 });
 
